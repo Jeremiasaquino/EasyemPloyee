6a9d4b0e13271d1e31fdb34a74f98cff2e3cbdae
Jeremi@s0313 <jeremiasaquino.m@gmail.com>
2023-08-10 23:45:34 -0400

    qq

--- app/Http/Controllers/NominaController.php
+++ app/Http/Controllers/NominaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Nomina;
 use App\Models\Empleado;
+use App\Models\Deducciones;
+use App\Models\Beneficios;
+use App\Models\Prestamos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -33,38 +36,44 @@ class NominaController extends Controller
         $validator = Validator::make($request->all(), [
             'fecha_nomina' => 'required|date',
             'empleado_id' => 'required|exists:empleados,id',
+            'hora_extra' => 'numeric',
+            'total_beneficios' => 'numeric',
+            'total_deducciones' => 'numeric',
+            'total_prestamos_adelanto' => 'numeric',
+            'cuenta_bancaria' => 'nullable|string',
+            'salario_neto' => 'numeric',
         ]);
-    
+
         if ($validator->fails()) {
             return response()->json(['success' => false, 'errors' => $validator->errors()], 400);
         }
-    
-        $empleado = Empleado::find($request->empleado_id);
-    
+
+        $empleado = Empleado::with(['Beneficios', 'Deducciones', 'Prestamos'])->find($request->empleado_id);
+
         if (!$empleado) {
             return response()->json([
                 'success' => false,
                 'message' => 'Empleado no encontrado.',
             ], 404);
         }
-    
+
         // Calcular total de beneficios
-        $totalBeneficios = $empleado->Beneficios->sum('amount');
-    
+        $totalBeneficios = $empleado->Beneficios->isEmpty() ? 0 : $empleado->Beneficios->sum('amount');
+
         // Calcular total de deducciones
-        $totalDeducciones = $empleado->Deducciones->sum('amount');
-    
+        $totalDeducciones = $empleado->Deducciones->isEmpty() ? 0 : $empleado->Deducciones->sum('amount');
+
         // Calcular total de préstamos por adelantado
-        $totalPrestamosAdelanto = $empleado->Prestamos->sum('amount');
-    
+        $totalPrestamosAdelanto = $empleado->Prestamos->isEmpty() ? 0 : $empleado->Prestamos->sum('amount');
+
         // Calcular salario neto
         $salarioNeto = $empleado->salario + $request->hora_extra + $totalBeneficios - $totalDeducciones - $totalPrestamosAdelanto;
-    
+
         // Calcular método de pago y cuenta bancaria
         $metodoPago = $empleado->cuenta_bancaria ? 'Transferencia' : 'Cheque';
         $cuentaBancaria = $empleado->cuenta_bancaria;
-    
-        $datosNominas = [
+
+        $datosNomina = [
             'empleado_id' => $empleado->id,
             'salario' => $empleado->salario,
             'hora_extra' => $request->hora_extra,
@@ -74,10 +83,11 @@ class NominaController extends Controller
             'salario_neto' => $salarioNeto,
             'metodo_pago' => $metodoPago,
             'cuenta_bancaria' => $cuentaBancaria,
-            'fecha_nomina' => $request->fecha_nomina, // Asegúrate de agregar la fecha
+            'fecha_nomina' => $request->fecha_nomina,
         ];
-    
-        $nomina = Nomina::create($datosNominas);
+
+        $nomina = Nomina::create($datosNomina);
+
         return response()->json(['success' => true, 'data' => $nomina]);
     }
 }
